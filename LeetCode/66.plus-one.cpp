c25575634594c4be7cb740d8ab645a8066eeb68f
Yokesh Rana <yokeshrana@gmail.com>
2020-12-16 00:58:43 +0530

    Plus One
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

---
+++ LeetCode/66.plus-one.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=66 lang=cpp
+ *
+ * [66] Plus One
+ */
+#include "stdc++.h"
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& nums) {
+        
+        int carry=1;
+        for(int i=nums.size()-1;i>=0;i--)
+            if(nums[i]+carry>9){
+                nums[i]=0;
+                carry=1;
+            }else
+            {
+                nums[i]+=carry;
+                return nums;
+            }
+        
+        nums.insert(nums.begin(),1);
+        return nums;
+    }
+};
+// @lc code=end
+
