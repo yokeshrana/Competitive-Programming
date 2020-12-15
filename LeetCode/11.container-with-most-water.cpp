c9bab3d267ca3e3a20c03d45bb26910fcaf1289d
Yokesh Rana <yokeshrana@gmail.com>
2020-12-15 23:16:33 +0530

    Container With Most Water
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

---
+++ LeetCode/11.container-with-most-water.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=11 lang=cpp
+ *
+ * [11] Container With Most Water
+ */
+#include "stdc++.h"
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int start = 0, end = height.size() - 1;
+        int area = 0;
+        while (start < end)
+        {
+            area = max(area, (end - start) * min(height[start], height[end]));
+            if (height[start] > height[end])
+            {
+                end--;
+            }
+            else
+            {
+                start++;
+            }
+        }
+        return area;
+    }
+};
+// @lc code=end
