95a55a71fe683178ec9cbc9fe299217a6e7d5758
Yokesh Rana <yokeshrana@gmail.com>
2020-12-16 20:32:12 +0530

    Linked List Cycle
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

---
+++ LeetCode/141.linked-list-cycle.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=141 lang=cpp
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+using namespace std;
+class Solution {
+public:
+    struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+    bool hasCycle(ListNode *head) {
+     ListNode *fast=head,*slow=head;
+
+       while(slow&&slow->next&&fast&&fast->next&&fast->next->next)
+       {
+           fast=fast->next->next;
+           slow=slow->next;
+           if(fast==slow)
+               return true;
+       }
+    return false;
+    }
+};
+// @lc code=end
+
