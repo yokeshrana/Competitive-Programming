33184bff31b7ba26c0112be3820644c8b5a9d90b
Yokesh Rana <yokeshrana@gmail.com>
2020-12-16 20:33:29 +0530

    House Robber
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

---
+++ LeetCode/198.house-robber.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=198 lang=cpp
+ *
+ * [198] House Robber
+ */
+#include<vector>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+   
+    int rob(vector<int>& nums) {
+    vector<int> dp(nums.size(),0);
+    if(nums.size() == 0) return 0;
+    if(nums.size() == 1) return nums[0];
+    dp[0]=nums[0];
+    dp[1]=max(nums[0],nums[1]);
+
+    for(int i=2;i<nums.size();i++){
+        dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+    }
+     
+    return dp[nums.size()-1];
+}
+};
+// @lc code=end
+
