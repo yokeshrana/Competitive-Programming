7c6616846c91f5c9723e2e784ca2ad57a4bbe32f
Yokesh Rana <yokeshrana@gmail.com>
2020-12-16 20:30:02 +0530

    Single Number
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

---
+++ LeetCode/136.single-number.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=136 lang=cpp
+ *
+ * [136] Single Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
