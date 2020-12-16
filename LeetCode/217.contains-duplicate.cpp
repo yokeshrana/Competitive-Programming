fd69aa28c2a388ea4c6498bbbdf9ff0101a4af86
Yokesh Rana <yokeshrana@gmail.com>
2020-12-16 20:38:19 +0530

    Contains Duplicate
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

---
+++ LeetCode/217.contains-duplicate.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=217 lang=cpp
+ *
+ * [217] Contains Duplicate
+ */
+#include "stdc++.h"
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s(nums.begin(),nums.end());
+        if(s.size()!=nums.size())return true ;else return false;
+    }
+};
+// @lc code=end
+
