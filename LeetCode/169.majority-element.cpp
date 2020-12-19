cf92a77b749aa8f6fea1ef4f810b6a0e2adbba37
Yokesh Rana <yokeshrana@gmail.com>
2020-12-19 09:34:32 +0530

    Majority element ,Using Divide and Connquer
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

--- LeetCode/169.majority-element.cpp
+++ LeetCode/169.majority-element.cpp
@@ -9,23 +9,29 @@ using namespace std;
 class Solution
 {
 public:
+    int count(vector<int> &nums, int element, int low, int high)
+    {
+        int c = 0;
+        for (int i = low; i <= high; i++)
+            if (nums[i] == element)
+                c++;
+        return c;
+    }
+    int findmajority(vector<int> &nums, int low, int high)
+    {
+        if (low == high) //Single elemnt case
+            return nums[low];
+        int mid = (high - low) / 2 + low; 
+        int left = findmajority(nums, low, mid);
+        int right = findmajority(nums, mid + 1, high);
+        if (left == right)
+            return left;
+        return count(nums, left, low, high) > count(nums, right, low, high) ? left : right;
+    }
     int majorityElement(vector<int> &nums)
     {
-        map<int, int> mp;
-        int max_in_mp = 0;
-        int element;
-        for (int i = 0; i < nums.size(); i++)
-            {
-                mp[nums[i]]++;
-                if (mp[nums[i]] > max_in_mp)
-                {
-                    max_in_mp = mp[nums[i]];
-                    element = nums[i];
-                    if(max_in_mp>nums.size()/2)
-                    return element;
-                }
-            }
-        return element;
+        int low = 0, high = nums.size() - 1;
+        return findmajority(nums, low, high);
     }
 };
 // @lc code=end
