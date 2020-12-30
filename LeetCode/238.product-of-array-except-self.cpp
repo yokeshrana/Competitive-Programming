5fa42f305d6d61b4d58e47e8eb0e6de1a95ca040
Yokesh Rana <yokeshrana@gmail.com>
2020-12-31 00:15:38 +0530

    Product of array except self :: Optimized Using left right pass
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

--- LeetCode/238.product-of-array-except-self.cpp
+++ LeetCode/238.product-of-array-except-self.cpp
@@ -11,51 +11,23 @@ class Solution
 public:
     vector<int> productExceptSelf(vector<int> &nums)
     {
-        long long int x = 1;
-        vector<int> zi;
-        for (int i = 0; i < nums.size(); i++)
-            if (nums[i] == 0)
-                zi.push_back(i);
-            else
-            {
-                x *= nums[i];
-            }
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] == 0)
-            {
-                if (zi.size() >= 1)
-                {
-                    if (zi.size() == 1 && i == zi[0])
-                        nums[i] = x;
-                    else
-                    {
-                        nums[i] = 0;
-                    }
-                }else
-                {
-                    nums[i]=x;
-                }
-                
-            }
-            else
-            {
-                if (zi.size() >= 1)
-                {
-                    if (zi.size() == 1 && i == zi[0])
-                        nums[i] = x;
-                    else
-                    {
-                        nums[i] = 0;
-                    }
-                }else
-                {
-                    nums[i]=x/nums[i];
-                }
-                
-            }
-        }
-        return nums;
+    //We solve it in two passes in first pass fill the array with left part multiples prefix
+    //in second pass will fill the array with right side multiple siffix multipled with content of left side multiple prefix computed in first step
+    vector<int> temp(nums.size(),1);
+    for(int i=1;i<nums.size();i++)
+    {
+        temp[i]=temp[i-1]*nums[i-1];
+    }
+    // ie if arry is 2,4,6,8 it will store
+    //               1 2 8 48
+    int rmul=1;
+    for(int i=nums.size()-1;i>=0;i--)
+    {
+        temp[i]*=rmul;
+        rmul*=nums[i];
+    }
+    // ie              192 96 64 48
+    return temp;
     }
 };
 // @lc code=end
