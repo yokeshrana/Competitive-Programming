53929a12cc5b9e0673a2617892beaf4d89db29db
Yokesh Rana <yokeshrana@gmail.com>
2021-01-08 01:09:10 +0530

    Squares of a Sorted Array O(n) approach with O(n) space
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

--- LeetCode/977.squares-of-a-sorted-array.cpp
+++ LeetCode/977.squares-of-a-sorted-array.cpp
@@ -3,18 +3,31 @@
  *
  * [977] Squares of a Sorted Array
  */
-
+#include <bits/stdc++.h>
+using namespace std;
 // @lc code=start
 class Solution
 {
 public:
     vector<int> sortedSquares(vector<int> &nums)
     {
-        for (int i = 0; i < nums.size(); i++)
-            nums[i] *= nums[i];
-
-        sort(nums.begin(), nums.end());
-        return nums;
+        vector<int> x(nums.size());
+        int k = nums.size() - 1;
+        int i = 0, j = k;
+        while (i <= j)
+        {
+            if (abs(nums[i]) > abs(nums[j]))
+            {
+                x[k--] = nums[i] * nums[i];
+                i++;
+            }
+            else
+            {
+                x[k--] = nums[j] * nums[j];
+                j--;
+            }
+        }
+        return x;
     }
 };
 // @lc code=end
