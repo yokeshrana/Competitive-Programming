3d3c4962a47741c4f93fa85dec650b9dc14ed330
Yokesh Rana <yokeshrana@gmail.com>
2021-01-10 12:18:24 +0530

    Intersection of Two Linked Lists
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

---
+++ LeetCode/160.intersection-of-two-linked-lists.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=160 lang=cpp
+ *
+ * [160] Intersection of Two Linked Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        int len1 = 0;
+        int len2 = 0;
+        ListNode *tmp = headA;
+        while (tmp)
+        {
+            tmp = tmp->next;
+            len1++;
+        }
+        tmp = headB;
+        while (tmp)
+        {
+            tmp = tmp->next;
+            len2++;
+        }
+        cout << len1 << " " << len2 << endl;
+        while (len1 > len2)
+        {
+            headA = headA->next;
+            len1--;
+        }
+        while (len2 > len1)
+        {
+            headB = headB->next;
+            len2--;
+        }
+        cout << len1 << " " << len2 << endl;
+        // cout << headA->val << " " << headB->val << endl;
+        while (headA && headB)
+        {
+            if (headA == headB)
+                return headA;
+            headB = headB->next;
+            headA = headA->next;
+        }
+        return nullptr;
+    }
+};
+// @lc code=end
