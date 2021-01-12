3dd0cc4c2eb433a49fa4a4059481909b8eb2a2aa
Yokesh Rana <yokeshrana@gmail.com>
2021-01-12 22:22:08 +0530

    Inorder Traversal Non Recursive Approach
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

--- LeetCode/94.binary-tree-inorder-traversal.cpp
+++ LeetCode/94.binary-tree-inorder-traversal.cpp
@@ -3,35 +3,41 @@
  *
  * [94] Binary Tree Inorder Traversal
  */
-
+#include <bits/stdc++.h>
+using namespace std;
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution
 {
 public:
-    void inorder(vector<int> &result, TreeNode *root)
-    {
-        if (root)
-        {
-            inorder(result, root->left);
-            result.push_back(root->val);
-            inorder(result, root->right);
-        }
-    }
     vector<int> inorderTraversal(TreeNode *root)
     {
         vector<int> result;
-        inorder(result, root);
+        stack<TreeNode *> s;
+        while (1)
+        {
+            while (root)
+            {
+                s.push(root);
+                root = root->left;
+            }
+            if (s.empty())
+                break;
+            root = s.top();
+            s.pop();
+            result.push_back(root->val);
+            root = root->right;
+        }
         return result;
     }
 };
