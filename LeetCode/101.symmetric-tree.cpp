0fcddfa6b07d296b396ca23f6c4f321a34aadafe
Yokesh Rana <yokeshrana@gmail.com>
2021-01-14 18:45:40 +0530

    Symmetric Tree Itreative Approach
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

--- LeetCode/101.symmetric-tree.cpp
+++ LeetCode/101.symmetric-tree.cpp
@@ -19,17 +19,29 @@
 class Solution
 {
 public:
-    bool isMirror(TreeNode *t1, TreeNode *t2)
-    {
-        if (!t1 && !t2)
-            return true;
-        if (!t1 || !t2)
-            return false;
-        return t1->val == t2->val && isMirror(t1->left, t2->right) && isMirror(t1->right, t2->left);
-    }
     bool isSymmetric(TreeNode *root)
     {
-        return isMirror(root, root);
+        queue<TreeNode *> q;
+        q.push(root);
+        q.push(root);
+        while (!q.empty())
+        {
+            TreeNode *t1 = q.front();
+            q.pop();
+            TreeNode *t2 = q.front();
+            q.pop();
+            if (!t1 && !t2)
+                continue;
+            if (!t1 || !t2)
+                return false;
+            if (t1->val != t2->val)
+                return false;
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        return true;
     }
 };
 // @lc code=end
