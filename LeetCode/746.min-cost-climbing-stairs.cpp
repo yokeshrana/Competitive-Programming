05aea477fddcdcfa3ba08c72fd619840274283f9
Yokesh Rana <yokeshrana@gmail.com>
2021-01-24 21:55:44 +0530

    Min Cost Climbing Stairs Dp
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

---
+++ LeetCode/746.min-cost-climbing-stairs.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=746 lang=cpp
+ *
+ * [746] Min Cost Climbing Stairs
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        vector<int> dp(cost.size());
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.size(); i++)
+        {
+            dp[i] = min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
+        }
+        return min(dp[cost.size() - 1], dp[cost.size() - 2]);
+    }
+};
+// @lc code=end
