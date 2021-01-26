b858630dd68079c9123743274d848c4616e85c93
Yokesh Rana <yokeshrana@gmail.com>
2021-01-27 02:15:18 +0530

    Compare LCS timing in recursive , Bottom up and top down
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

--- Implementation/DP/LCS.cpp
+++ Implementation/DP/LCS.cpp
@@ -10,11 +10,11 @@ int lcs_recur(string x, string y, int m, int n)
         return max(lcs_recur(x, y, m, n - 1), lcs_recur(x, y, m - 1, n));
 }
 
-vector<vector<int>> dp(8, vector<int>(5, -1));
 int lcs_dp_td(string x, string y, int m, int n)
 {
+    static vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
     if (m == 0 || n == 0)
-        return 0;
+        return dp[m][n] = 0;
     if (dp[m][n] != -1)
         return dp[m][n];
     if (x[m - 1] == y[n - 1])
@@ -44,9 +44,16 @@ int lcs_dp_bu(string x, string y, int m, int n)
 
 int main()
 {
-    string x = "xcasfrt", y = "xsrt";
+    string x = "xcasfrt", y = "xsrtefg";
+    auto t1 = std::chrono::high_resolution_clock::now();
     cout << lcs_recur(x, y, x.length(), y.length()) << endl;
+    auto t2 = std::chrono::high_resolution_clock::now();
+    cout << "Time taken by function: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " microseconds" << endl;
     cout << lcs_dp_td(x, y, x.length(), y.length()) << endl;
+    auto t3 = std::chrono::high_resolution_clock::now();
+    cout << "Time taken by function: " << std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count() << " microseconds" << endl;
     cout << lcs_dp_bu(x, y, x.length(), y.length()) << endl;
+    auto t4 = std::chrono::high_resolution_clock::now();
+    cout << "Time taken by function: " << std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count() << " microseconds" << endl;
     return 0;
 }
