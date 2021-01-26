002a2a572a0c8c13ce8bc0edd1a2c568fde80d12
Yokesh Rana <yokeshrana@gmail.com>
2021-01-27 01:56:13 +0530

    LCS DP
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

---
+++ Implementation/DP/LCS.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+int lcs_recur(string x, string y, int m, int n)
+{
+    if (m == 0 || n == 0)
+        return 0;
+    if (x[m - 1] == y[n - 1])
+        return 1 + lcs_recur(x, y, m - 1, n - 1);
+    else
+        return max(lcs_recur(x, y, m, n - 1), lcs_recur(x, y, m - 1, n));
+}
+
+vector<vector<int>> dp(8, vector<int>(5, -1));
+int lcs_dp_td(string x, string y, int m, int n)
+{
+    if (m == 0 || n == 0)
+        return 0;
+    if (dp[m][n] != -1)
+        return dp[m][n];
+    if (x[m - 1] == y[n - 1])
+        return dp[m][n] = 1 + lcs_recur(x, y, m - 1, n - 1);
+    else
+        return dp[m][n] = max(lcs_recur(x, y, m, n - 1), lcs_recur(x, y, m - 1, n));
+}
+
+int lcs_dp_bu(string x, string y, int m, int n)
+{
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+    for (int i = 0; i <= m; i++)
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+                continue;
+            }
+            if (x[i - 1] == y[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    return dp[m][n];
+}
+
+int main()
+{
+    string x = "xcasfrt", y = "xsrt";
+    cout << lcs_recur(x, y, x.length(), y.length()) << endl;
+    cout << lcs_dp_td(x, y, x.length(), y.length()) << endl;
+    cout << lcs_dp_bu(x, y, x.length(), y.length()) << endl;
+    return 0;
+}
