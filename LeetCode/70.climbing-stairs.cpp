3065f18ae4c045a34198d7dacbce1f883261f863
Yokesh Rana <yokeshrana@gmail.com>
2021-01-31 20:09:10 +0530

    Climbing Stairs
    Signed-off-by: Yokesh Rana <yokeshrana@gmail.com>

--- LeetCode/70.climbing-stairs.cpp
+++ LeetCode/70.climbing-stairs.cpp
@@ -3,38 +3,50 @@
  *
  * [70] Climbing Stairs
  */
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    
-    int climb(int n,vector<int> &dp){
-        if(n==1) return 1;
-        if(n==2) return 2;
-        if(n<=0) return 0;
-        int a,b;
-        if(dp[n-2]==0)
-        {
-            a=climb(n-2,dp);
-            dp[n-2]=a;
-        }else
-            a=dp[n-2];
-        if(dp[n-1]==0)
-        {
-            b=climb(n-1,dp);
-            dp[n-1]=b;
-        }else
-            b=dp[n-1];
-        return a+b;
-            
+    int climb_rec(int n) //O(2^n)
+    {
+        if (n <= 2)
+            return n;
+        else
+            return climb_rec(n - 1) + climb_rec(n - 2);
     }
-    
-    
-    int climbStairs(int n) {
-        vector<int> dp(n+1,0);
-        return climb(n,dp);
+    int climb_top_down(vector<int> &dp, int n) //O(n)
+    {
+
+        if (n <= 2)
+            dp[n] = n;
+        if (dp[n] != -1)
+            return dp[n];
+        else
+        {
+            return dp[n] = climb_top_down(dp, n - 1) + climb_top_down(dp, n - 2);
+        }
+    }
+    int climb_bottom_up(int n) //O(n)
+    {
+        vector<int> dp(n + 1, -1);
+        dp[1] = 1;
+        if (n == 1)
+            return dp[1];
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+    int climbStairs(int n)
+    {
+        //return climb_rec(n);
+
+        //vector<int> dp(n + 1, -1);
+        //return climb_top_down(dp, n);
+
+        return climb_bottom_up(n);
     }
 };
 // @lc code=end
-
