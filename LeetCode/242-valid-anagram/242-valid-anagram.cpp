1c80df9029b7ff38229f386f645190534b61009d
Yokesh Rana <yokeshrana@gmail.com>
2022-05-04 01:26:47 +0530

    [cpp] : 242-valid-anagram

---
+++ LeetCode/242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        map<char,int> mp;
+        for(int i=0;i<s.length();i++){
+           mp[s[i]]++;
+        }
+        
+        for(int i=0;i<t.length();i++){
+           if(!mp[t[i]])
+               return false;
+            else
+            {
+                mp[t[i]]--; 
+                if(mp[t[i]]<0)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
