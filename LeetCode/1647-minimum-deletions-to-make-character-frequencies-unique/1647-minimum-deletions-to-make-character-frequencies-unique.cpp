56e94576ef99ecd4a8b7f0331c3312b650e11e61
Yokesh Rana <yokeshrana@gmail.com>
2022-06-30 18:50:38 +0530

    [cpp] : 1647-minimum-deletions-to-make-character-frequencies-unique

--- LeetCode/1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ LeetCode/1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int minDeletions(string s) {
-         map<char,int> mp;
-        int count=0;   
-        for(char c:s)
-            mp[c]++;
-        set<int> seen;
-        for(auto it=mp.begin();it!=mp.end();it++){
-            while(seen.find(it->second)!=seen.end())
-            {
-                (it->second)--;
+   
+   vector<int> freq(26,0);
+    for(auto &v:s){
+        freq[v-'a']++;
+    }
+    
+    priority_queue<int> pq;
+    for(auto x:freq){if(x>0) pq.push(x);}
+    
+    int count=0;
+        while(pq.size()>1){
+            int top=pq.top();
+            pq.pop();
+            if(top==pq.top()){
+                //making sure that we dont push any negative val
+                if(top-1>0)
+                pq.push(top-1);
                 count++;
             }
-            if(it->second>0)
-            seen.insert(it->second);
         }
-        return count;
+       
+       return count;
+        
     }
 };
