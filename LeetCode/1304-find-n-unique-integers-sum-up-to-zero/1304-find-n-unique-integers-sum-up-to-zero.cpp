f6b699ba22cadc934713973e137305ee7cd2064f
Yokesh Rana <yokeshrana@gmail.com>
2022-07-02 02:26:10 +0530

    [cpp] : 1304-find-n-unique-integers-sum-up-to-zero

--- LeetCode/1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
+++ LeetCode/1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> sumZero(int n) {
-       vector<int> a(n);
-        for(int i=1;i<a.size();i++){
-            a[0]-=i;
-            a[i]=i;
-        }
-        return a;
+       vector<int> ans(n,0);
+       ans[0]=0;
+       for(int i=1;i<n;i++)
+       {
+           ans[0]-=i;
+           ans[i]=i;
+       }
+      return ans;
     }
 };
